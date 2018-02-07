701b5935ea775491b9b4817f390a87a33cf2c556
Jowe11 <joseph.rowe@web-developer.work>
2018-02-07 06:46:00 -0700

    annoying alert loop

---
+++ annoy.js
@@ -0,0 +1,15 @@
+var answer = prompt("Are we there yet?")
+
+// while (answer !== "yes" && answer !== "yeah") {
+//     var answer = prompt("Are we there yet?")
+// }
+
+// alert("Yay we made it!");
+
+//Version 2
+
+while (answer.indexOf("yes") == -1) {
+    var answer = prompt("Are we there yet?");
+}
+
+alert("Yay, We make it!!!");
